8a09d2af49dad75730a2ec5467803fa19d804c66
Usevalad Shychanin <exporesource@gmail.com>
2020-09-09 01:57:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : Math.min(...array) 
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return 0
+  const sum = array.reduce((p, n) => p+n, 0)
+  return sum / array.length
 }
